dee7a0f2316173dde19e13fe8abed544671f83b3
Sachin Khargie <sachx1281@gmail.com>
2021-12-07 14:49:16 -0500

    added firebase logo to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './footer.scss'
 import { FaGithub } from 'react-icons/fa';
+import { IoLogoFirebase } from 'react-icons/io5' 
 
 const footer = () => {
     return (
         <div className="footer">
-            <a href="https://github.com/sachx1/portfolio"><button className="footerButton"><FaGithub /> Built and Designed by Sachin Khargie</button></a>
+            <a href="https://github.com/sachx1/portfolio"><button className="footerButton"><FaGithub /> Built and Designed by Sachin Khargie. Hosted on Google Firebase <IoLogoFirebase /></button></a>
         </div>
     )
 }
