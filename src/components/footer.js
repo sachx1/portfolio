442c19ecae397fd4cb34fdb421f6cf1edeee7ff4
Sachin Khargie <sachx1281@gmail.com>
2021-12-07 14:37:00 -0500

    updated footer and contact sections

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './footer.scss'
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 const footer = () => {
     return (
         <div className="footer">
             <a href="https://github.com/sachx1/portfolio"><button className="footerButton"><FaGithub /> Built and Designed by Sachin Khargie</button></a>
-            <a href="https://www.linkedin.com/in/sachin-khargie-17a248163/"><button className="footerButton"><FaLinkedin /></button></a>
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai'
 
 function Contact() {
     return (
@@ -8,7 +10,9 @@ function Contact() {
             </div>
             <div className='containerHome'>
                 {/* <h1><a className="mailto" href="mailto:sachin.khargie@gmail.com">Contact Me</a></h1> */}
-               <a className="mailto" href="mailto:sachin.khargie@gmail.com"><button className="button button5">Contact Me</button></a>
+               <a className="mailto" href="mailto:sachin.khargie@gmail.com"><button className="button button5"><AiOutlineMail /> Email</button></a>
+               <a href="https://www.linkedin.com/in/sachin-khargie-17a248163/"><button className="button button5"><FaLinkedin /> LinkedIn</button></a>
+               <a href="https://github.com/sachx1"><button className="button button5"><FaGithub /> Github</button></a>
             </div>
         </>
     )
